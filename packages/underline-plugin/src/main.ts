33df5e1d1eb7beb94abf93683443effae5a538a6
Owen <189063419+cascaade@users.noreply.github.com>
2026-08-01 15:15:48 -0400

    Underline plugin

---
+++ packages/underline-plugin/src/main.ts
@@ -0,0 +1,58 @@
+import { Plugin, Editor, Notice } from "obsidian";
+
+export default class UnderlinePlugin extends Plugin {
+    async onload() {
+        this.addCommand({
+            id: "toggle-underline-html",
+            name: "Underline with <u> tags",
+            hotkeys: [
+                {
+                    modifiers: ["Mod"],
+                    key: "u",
+                },
+            ],
+            editorCallback: (editor: Editor) => {
+                const selection = editor.getSelection();
+
+                const from = editor.getCursor("from");
+                const to = editor.getCursor("to");
+
+                const before = editor.getRange(
+                    { line: from.line, ch: Math.max(0, from.ch - 3) },
+                    from
+                );
+
+                const after = editor.getRange(
+                    to,
+                    { line: to.line, ch: to.ch + 4 }
+                );
+
+                if (before === "<u>" && after === "</u>") {
+                    // Remove surrounding tags
+                    editor.replaceRange("", to, { line: to.line, ch: to.ch + 4 });
+                    editor.replaceRange(
+                        "",
+                        { line: from.line, ch: from.ch - 3 },
+                        from
+                    );
+
+                    // Keep the same text selected
+                    editor.setSelection(
+                        { line: from.line, ch: from.ch - 3 },
+                        { line: to.line, ch: to.ch - 3 }
+                    );
+                } else {
+                    // Add surrounding tags
+                    editor.replaceRange("</u>", to);
+                    editor.replaceRange("<u>", from);
+
+                    // Keep the original text selected
+                    editor.setSelection(
+                        { line: from.line, ch: from.ch + 3 },
+                        { line: to.line, ch: to.ch + 3 }
+                    );
+                }
+            }
+        });
+    }
+}
