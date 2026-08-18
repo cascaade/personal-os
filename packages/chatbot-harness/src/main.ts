0a530166c687c7d382de8961fdd66eb5180028ab
Owen <189063419+cascaade@users.noreply.github.com>
2026-08-17 22:42:11 -0400

    Basic react server

--- packages/chatbot-harness/src/main.ts
+++ packages/chatbot-harness/src/main.ts
@@ -1,1 +1,18 @@
-console.log("hello world!");
+import dotenv from "dotenv";
+import express from 'express';
+import morgan from "morgan";
+
+const app = express();
+
+dotenv.config();
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Listening on port", process.env.PORT);
+});
